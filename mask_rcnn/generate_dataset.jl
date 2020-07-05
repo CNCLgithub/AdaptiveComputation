c8db3cb22d7c5ff51c4e8a377db97d6571a9782b
eivinasbutkus <eivinas.butkus@yale.edu>
2020-07-05 15:10:44 -0400

    generating a harder dataset

--- mask_rcnn/generate_dataset.jl
+++ mask_rcnn/generate_dataset.jl
@@ -4,8 +4,17 @@ using JSON
 using Random
 
 dataset_path = joinpath("output", "datasets", "mask_rcnn")
-n_batches = 128
-n_examples = 40
+mkpath(dataset_path)
+#n_batches = 200
+n_examples = 10
+
+spring = LinRange(0.0005, 0.0025, 10)
+sigma_w = LinRange(1.0, 2.5, 10)
+n_dots = collect(8:16)
+
+batches = Iterators.product(spring, sigma_w, n_dots)
+n_batches = length(batches)
+
 # most of the action is later, so skipping first timesteps
 start_timestep = 61
 n_timesteps = 120
@@ -19,37 +28,43 @@ open(json_path, "w") do f
     write(f, data)
 end
 
-# note that 
 target_pngs_path = joinpath(dataset_path, "target_pngs")
 mkpath(target_pngs_path)
 
-for batch=1:n_batches
+for (batch, elements) in enumerate(batches)
     println("batch $batch")
     
+    if batch > 10
+        error()
+    end
     # sample some timesteps from the back of the trial
     timesteps = collect(start_timestep:n_timesteps)
     perm = randperm(length(timesteps))
     timesteps = timesteps[perm][1:n_examples]
     println("sampled timesteps: $timesteps")
+    
 
+    """
     q = Exp0(trial=batch)
-
     gm = MOT.load(GMMaskParams, q.gm)
     init_positions, masks, motion, positions = load_exp0_trial(batch, gm, q.dataset_path,
                                                                generate_masks=false)
+    """
     
+    spring, sigma_w, n_trackers = elements
+    gm = GMMaskParams(n_trackers = n_trackers,
+                      distractor_rate = 0,
+                      init_pos_spread = 200)
+    motion = BrownianDynamicsModel(spring=spring, sigma_x=sigma_w, sigma_y=sigma_w)
+    init_positions, init_vels, masks, positions = dgp(n_timesteps, gm, motion)
+
+
     # replacing z with y for a fixed ordering based on y
     for t=1:length(positions)
         for i=1:size(positions[t],1)
             positions[t][i,3] = positions[t][i,2]
         end
     end
-    
-    """
-    gm = GMMaskParams(n_trackers = 4, distractor_rate = 4)
-    motion = BrownianDynamicsModel()
-    init_positions, init_vels, masks, positions = dgp(n_timesteps, gm, motion)
-    """
 
     # rendering the input images of the trial
     # and saving to trial path
