71436bad9d763483b906fec936cb09569bb313bc
belledon <mbelledonne@gmail.com>
2020-08-06 09:16:39 -0400

    now runs on milgram

--- scripts/analysis/attention_map.jl
+++ scripts/analysis/attention_map.jl
@@ -1,6 +1,6 @@
 using CSV
+using Statistics
 using DataFrames
-using ArgParse
 
 function attention_map(c::Dict)
     aux_state = c["aux_state"]
@@ -13,13 +13,15 @@ function attention_map(c::Dict)
     attended
 end
 
-function attention_maps(experiment::String)
-    for i = 1:124
-        trial_path = "$(experiment)/($i)"
+function attention_maps(experiment::String; n::Int = 128)
+    for i = 1:n
+        trial_path = "$(experiment)/$i"
         chain_paths = filter(x -> occursin("jld", x), readdir(trial_path))
+        chain_paths = map(x -> joinpath(trial_path, x), chain_paths)
         chains = map(extract_chain, chain_paths)
-        atts = collect(map(attention_map, chains))
-        atts = mean(atts, dims = 1)
+        atts = map(attention_map, chains)
+        atts = sum(atts) ./ length(chain_paths) 
+        display(atts)
         out_path = joinpath(trial_path, "$(i)_att.csv")
         CSV.write(out_path, DataFrame(atts), writeheader=false)
     end
