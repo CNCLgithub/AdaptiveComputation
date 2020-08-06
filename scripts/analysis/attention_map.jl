4092382c68a25ac1e5e7ec681aea7a8658e909a8
belledon <mbelledonne@gmail.com>
2020-08-06 09:00:22 -0400

    tweaked attention maps

--- scripts/analysis/attention_map.jl
+++ scripts/analysis/attention_map.jl
@@ -14,11 +14,12 @@ function attention_map(c::Dict)
 end
 
 function attention_maps(experiment::String)
-    for i = 1:120
+    for i = 1:124
         trial_path = "$(experiment)/($i)"
         chain_paths = filter(x -> occursin("jld", x), readdir(trial_path))
         chains = map(extract_chain, chain_paths)
-        atts = mean(map(attention_map, chains), dims = 1)
+        atts = collect(map(attention_map, chains))
+        atts = mean(atts, dims = 1)
         out_path = joinpath(trial_path, "$(i)_att.csv")
         CSV.write(out_path, DataFrame(atts), writeheader=false)
     end
