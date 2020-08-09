3e2ca33296cfe5fbc63daec19a53dbbe36aa6280
belledon <mbelledonne@gmail.com>
2020-08-09 11:07:45 -0400

    cleanup and added some att analysis

--- scripts/analysis/attention_map.jl
+++ scripts/analysis/attention_map.jl
@@ -1,6 +1,11 @@
+using MOT
 using CSV
 using Statistics
 using DataFrames
+using Gadfly
+Gadfly.push_theme(Theme(background_color = colorant"white"))
+using Compose
+import Cairo
 
 function attention_map(c::Dict)
     aux_state = c["aux_state"]
@@ -13,16 +18,38 @@ function attention_map(c::Dict)
     attended
 end
 
-function attention_maps(experiment::String; n::Int = 128)
+function load_attmap(path::String)
+    chain_paths = filter(x -> occursin("jld", x), readdir(trial_path))
+    chain_paths = map(x -> joinpath(trial_path, x), chain_paths)
+    chains = map(extract_chain, chain_paths)
+    atts = map(attention_map, chains)
+    sum(atts) ./ length(chain_paths)
+end
+
+function plot_attmap(att::Matrix{Float64}, path::String)
+    p = Gadfly.spy(att', Guide.xlabel("Time"), Guide.ylabel("Tracker"))
+    p |> PNG(path)
+end
+
+function compare_att(a::String, b::String, i::Int64;
+                             k::Int64 = 2)
+    a_i = load_attmap("$a/$i")
+    b_i = load_attmap("$b/$i")
+    ds = a_i .- b_i
+    d = map(norm, eachrow(ds))
+    ps = take(sortperm(d, rev = true), 2)
+    ds, ps
+end
+
+function compare_experiments(a::String, b::String; n::Int64 = 128)
+    out = "/experiments/$(a)_vs_$(b)"
+    isdir(out) || mkdir(out)
+    a_path = "/experiments/$a"
+    b_path = "/experiments/$b"
     for i = 1:n
-        trial_path = "$(experiment)/$i"
-        chain_paths = filter(x -> occursin("jld", x), readdir(trial_path))
-        chain_paths = map(x -> joinpath(trial_path, x), chain_paths)
-        chains = map(extract_chain, chain_paths)
-        atts = map(attention_map, chains)
-        atts = sum(atts) ./ length(chain_paths) 
-        display(atts)
-        out_path = joinpath(trial_path, "$(i)_att.csv")
-        CSV.write(out_path, DataFrame(atts), writeheader=false)
+        ds, ps = compare_att(a, b, i)
+        plot_attmap(ds, joinpath(out, "$i_att.png"))
+        CSV.write(joinpath(out, "$i.csv"), DataFrame(ds[ps,:]),
+                  writeheader=false)
     end
 end
--- scripts/analysis/performance_compute.jl
+++ scripts/analysis/performance_compute.jl
@@ -3,142 +3,27 @@ using MOT
 using HDF5
 using Gadfly
 using Statistics
-#using GLM
 using DataFrames
 using CSV
 
-#using Bootstrap
+function performance_compute(src::String)
+    results = load_results(src)
+    perf_trial = mean(results["performance"], dims=2)[:,1]
+    comp_trial = mean(results["compute"], dims=2)[:,1]
+    pred_target_trial = mean(results["pred_target"], dims=2)[:,1,:]
 
-num_trials = 128
-results_dir = "exp0_results"
-
-
-function performance_compute(experiments)
-    for experiment in experiments
-        # performance, compute, pred_target = load_results(experiment)
-        results = load_results(experiment)
-        
-        perf_trial = mean(results["performance"], dims=2)[:,1]
-        comp_trial = mean(results["compute"], dims=2)[:,1]
-        pred_target_trial = mean(results["pred_target"], dims=2)[:,1,:]
-
-        pred_target_packaged = []
-        for i=1:size(pred_target_trial, 1)
-            push!(pred_target_packaged, pred_target_trial[i,:])
-        end
-
-        trials = collect(1:length(perf_trial))
-
-        df = DataFrame(performance=perf_trial, compute=comp_trial,
-                       trial=trials;
-                       [Symbol("dot_$i")=>pred_target_trial[:,i] for i=1:8]...
-                      )
-
-        CSV.write("$(experiment)/performance_compute.csv", df)
-        println(df)
+    pred_target_packaged = []
+    for i=1:size(pred_target_trial, 1)
+        push!(pred_target_packaged, pred_target_trial[i,:])
     end
+
+    trials = collect(1:length(perf_trial))
+
+    df = DataFrame(performance=perf_trial, compute=comp_trial,
+                    trial=trials;
+                    [Symbol("dot_$i")=>pred_target_trial[:,i] for i=1:8]...
+                    )
+
+    CSV.write("$(src)/performance_compute.csv", df)
+    println(df)
 end
-
-performance_compute(["output/experiments/exp0_trial_avg"])
-
-
-function confidence_intervals(data; samples=false)
-	n_boot = 100000
-	cil = 0.95
-	bs = bootstrap(mean, data, BasicSampling(n_boot))
-	if samples
-		return bs.t1[1]
-	end
-	bci = confint(bs, PercentileConfInt(cil))[1]
-	return bci
-end
-
-
-function plot_performance()
-	num_targets = 4
-	num_observations = 8
-	num_trials = 128
-	model_names = deepcopy(models)
-
-	perf_plot = []
-	perf_plot_min = []
-	perf_plot_max = []
-
-	for i=1:length(models)
-		model = models[i]
-		println(model)
-		_, A, _, perf, comp = load_results_trials("$results_dir/$model")
-		
-		models[i] *= "\n compute $(mean(comp))\n perf $(mean(perf)/4)"
-		compute = mean(comp; dims=2)
-		println("maximum compute $(maximum(compute))")
-		perf /= num_targets
-		
-		perf = mean(perf, dims=2)
-		perf_mean, perf_min, perf_max = confidence_intervals(perf)
-		push!(perf_plot_min, perf_min)
-		push!(perf_plot_max, perf_max)
-		push!(perf_plot, perf_mean)
-	end
-
-	bars = Gadfly.layer(x=models, y=perf_plot,
-						color=model_names,
-						Geom.bar)
-	errorbars = Gadfly.layer(x=models,
-							 y=perf_plot,
-							ymin=perf_plot_min,
-							ymax=perf_plot_max,
-							Geom.errorbar,
-							Theme(default_color="black"))
-
-	df = DataFrame(models=model_names, y=perf_plot, ymin=perf_plot_min, ymax=perf_plot_max)
-	CSV.write("models_performance.csv", df)
-
-	performance_plots = "plots/performance_plots"
-	mkpath(performance_plots)
-	path = "$performance_plots/performance.svg"
-
-	p = Gadfly.plot(errorbars, bars,
-					Scale.y_continuous(minvalue=0.75, maxvalue=0.9),
-					Guide.xlabel("models"),
-					Guide.ylabel("performance"),
-					Theme(background_color="white"))
-
-	Gadfly.draw(SVG(path, 10Gadfly.inch, 6Gadfly.inch), p)
-end
-
-function significance_analysis(first, second)
-	_,A,_,perf_1,comp = load_results_trials("$results_dir/$first")
-	_,A,_,perf_2,comp = load_results_trials("$results_dir/$second")
-	perf_1 = mean(perf_1, dims=2)
-	perf_2 = mean(perf_2, dims=2)
-
-	first_samples = confidence_intervals(perf_1, samples=true)
-	second_samples = confidence_intervals(perf_2, samples=true)
-	println(1 - count(first_samples .- second_samples .> 0)/100000)
-end
-
-
-function plot_performance_between_models(model_1, model_2)
-    _,_,_,perf_1,_ = load_results_trials("$results_dir/$model_1")
-    _,_,_,perf_2,_ = load_results_trials("$results_dir/$model_2")
-
-    performance_1 = mean(perf_1, dims=2)[:,1]/4
-    performance_2 = mean(perf_2, dims=2)[:,1]/4
-
-    trials = ["$i" for i=1:num_trials]
-    
-    p = plot(x=performance_1, y=performance_2,
-             label=trials,
-             Guide.xlabel(model_1),
-             Guide.ylabel(model_2),
-             Theme(background_color="white"),
-             Geom.point,
-             Geom.label)
-    Gadfly.draw(PNG("performance_between_models.png", 16Gadfly.inch, 16Gadfly.inch), p)
-end
-
-#significance_analysis("rejuv", "no_rejuv_trial")
-#plot_performance()
-#accuracy_compute()
-#plot_performance_between_models("rejuv", "no_rejuv_trial")
