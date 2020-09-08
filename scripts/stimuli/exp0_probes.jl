8d0a3436b55dac3a94719829cafe22563be96572
belledon <mbelledonne@gmail.com>
2020-09-08 14:13:49 -0400

    working on exp0 probes

--- scripts/stimuli/exp0_probes.jl
+++ scripts/stimuli/exp0_probes.jl
@@ -20,26 +20,16 @@ function render_probe_trial(trial_row::DataFrameRow, out::String;
     trial = trial_row.scene + 1
     q = Exp0(k=120, trial = trial)
 
-    td_out = joinpath(out, "td")
-    td_args = Tuple(trial_row[[:td_tracker, :td_t]])
-    gm, pos, probes = place_probes(q, td_args..., pad)
-    render(gm, dot_positions = pos, probes = probes, path = td_out,
-           stimuli = true, highlighted = [1], freeze_time = 24)
-    compile_video(out, "td")
+    args = Tuple(trial_row[[:tracker, :frame]])
+    gm, pos, probes = place_probes(q, args..., pad)
+    render(gm, dot_positions = pos, probes = probes, path = out,
+           stimuli = true, highlighted = collect(1:4), freeze_time = 24)
+    compile_video(out)
 
-    dc_out = joinpath(out, "dc")
-    dc_args = Tuple(trial_row[[:dc_tracker, :dc_t]])
-    _, _, probes = place_probes(q, dc_args..., pad)
-    render(gm, dot_positions = pos, probes = probes, path = dc_out,
-           stimuli = true, highlighted = [1], freeze_time = 24)
-    compile_video(out, "dc")
-    display(td_args)
-    display(dc_args)
     return nothing
 end
 
-function compile_video(path::String, model::String)
-    render_path = joinpath(path, model)
+function compile_video(render_path::String)
     imgnames = filter(x->occursin(".png",x), readdir(render_path))
     intstrings =  map(x->split(x,".")[1],imgnames)
     p = sortperm(parse.(Int,intstrings))
@@ -47,7 +37,7 @@ function compile_video(path::String, model::String)
     for imgname in imgnames[p]
         push!(imgstack,load(joinpath(render_path, imgname)))
     end
-    encodevideo("$(path)_$(model).mp4", imgstack)
+    encodevideo("$(render_path).mp4", imgstack)
 end
 
 function render_probe_trials(att_tps::String)
@@ -55,9 +45,10 @@ function render_probe_trials(att_tps::String)
     ispath(out) || mkpath(out)
 
     df = DataFrame(CSV.File(att_tps))
-    for trial_row in eachrow(df)
+    display(df)
+    for (i, trial_row) in enumerate(eachrow(df))
         trial = trial_row.scene
-        trial_out = "$(out)/$(trial)"
+        trial_out = "$(out)/$i"
         ispath(trial_out) || mkpath(trial_out)
         render_probe_trial(trial_row, trial_out)
     end
