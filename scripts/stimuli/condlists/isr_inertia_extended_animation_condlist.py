378bd1aa6ae916785a4c2cb2a37b7f68925e37fa
eivinasbutkus <eivinas.butkus@yale.edu>
2020-11-06 16:52:07 -0500

    condlist generation for 4 probes (2 low, 2 high)

--- scripts/stimuli/condlists/isr_inertia_extended_animation_condlist.py
+++ scripts/stimuli/condlists/isr_inertia_extended_animation_condlist.py
@@ -4,6 +4,7 @@ import os
 import json
 import argparse
 import pandas as pd
+import numpy as np
 
 def generate_condlist(scenes, data, outpath):
     
@@ -12,26 +13,27 @@ def generate_condlist(scenes, data, outpath):
     # just one condition for now
     for cond in range(1):
         cond_trials = []
-
-        low = 0
+        
+        angle = 0 
 
         for (i, scene) in enumerate(scenes):
-            angle = 0
+            low = np.random.permutation(4)[:2]
 
             # mapping tracker_trial to the scene's trackers
             scene_data = data[data.scene == scene]
+            print(scene_data)
             frames = scene_data.frame.unique()
 
             scene_probes = []
             for (j, frame) in enumerate(frames):
-                frame_row = int(not j == low)
-                idx = i*6 + j*2 + frame_row
+                frame_row = int(not j in low)
+                idx = i*8 + j*2 + frame_row
                 tracker_data = scene_data.loc[idx]
                 scene_probes.append([int(tracker_data['tracker']), int(tracker_data['frame'])])
 
             cond_trials.append([scene, angle, scene_probes])
 
-            low = (low + 1) % 3
+            #low = (low + 1) % 3
 
         condlist.append(cond_trials)
     
@@ -48,8 +50,7 @@ def main():
         formatter_class = argparse.ArgumentDefaultsHelpFormatter
     )
     
-    # movies_path = '/home/eivinas/dev/mot-psiturk/psiturk/static/data/movies'
-    data = pd.read_csv('output/attention_analysis/isr_inertia_extended_probe_map_nd.csv')
+    data = pd.read_csv('output/attention_analysis/isr_inertia_480_probe_map.csv')
 
     scenes = data.scene.unique().tolist()
     generate_condlist(scenes, data, os.path.join('/renders', 'condlist.json'))
