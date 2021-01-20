9894bc44c5fd07535b9f5d9d4aed652efb63ff74
eivinasbutkus <eivinas.butkus@yale.edu>
2021-01-20 17:42:02 -0500

    deleting unnecessary stuff

--- setup.sh
+++ setup.sh
@@ -25,37 +25,9 @@ supported targets:
 # conda setup
 [[ "${@}" =~ "conda" ]] || echo "Not touching conda"
 [[ "${@}" =~ "conda" ]] && echo "building conda env" && \
-    singularity exec ${ENV[cont]} bash -c "yes | conda create -p $PWD/${ENV[env]} python=3.6" && \
-    ./run.sh python -m pip install -r requirements.txt && \
-    # detectron2 setup (dependent on torch, so out of the requirements.txt
-    # since pip installs in the alphabetical order)
-    ./run.sh python -m pip install -e deps/detectron2
+    singularity exec ${ENV[cont]} bash -c "yes | conda create -p $PWD/${ENV[env]} python=3.6"
 
 # julia setup
 [[ "${@}" =~ "julia" ]] || echo "Not touching julia"
 [[ "${@}" =~ "julia" ]] && echo "building julia env" && \
     ./run.sh julia -e '"using Pkg; Pkg.instantiate()"'
-
-# datasets
-[[ "${@}" =~ "datasets" ]] || [[ "${@}" =~ "datasets" ]] || echo "Not touching datasets"
-[[ "${@}" =~ "datasets" ]] && echo "pulling datasets" && \
-    mkdir -p output/datasets && \
-    echo "pulling exp_0 dataset" && \
-    wget "https://yale.box.com/shared/static/2kt5psxh7nyb5s3s09g4kwhxnjmigcjs.h5" \
-        -O "output/datasets/exp_0.h5" && \
-    wget "https://yale.box.com/shared/static/0jeyo01zkm4viwmqsm9qzdy3uatbd755.jld2" \
-        -O "output/datasets/exp0.jld2" && \
-    wget "https://yale.box.com/shared/static/ikq1e26l8xbhrac8x3cxwei972g0judo.jld2" \
-        -O "output/datasets/exp1.jld2" && \
-    wget "https://yale.box.com/shared/static/75accvgspil2sr34tg2g3e37uvhjkxz6.jld2" \
-        -O "output/datasets/exp1_isr.jld2" && \
-    wget "https://yale.box.com/shared/static/wwpwkh7ara5wqmr8bde3jvvdael8x783.jld2" \
-        -O "output/datasets/exp1_brownian.jld2"
-
-# checkpoints
-[[ "${@}" =~ "checkpoints" ]] || [[ "${@}" =~ "checkpoints" ]] || echo "Not touching checkpoints"
-[[ "${@}" =~ "checkpoints" ]] && echo "pulling checkpoints" && \
-    mkdir -p checkpoints && \
-    echo "pulling mask_rcnn_weights_0" && \
-    wget "https://yale.box.com/shared/static/85eiwmnthuy93bqrpwb5hwdo70qndmbk.pth" \
-    -O "output/checkpoints/mask_rcnn_weights_0.pth"
