9a2312f8f1c213986e472f28a3db31d5c0cf3578
belledon <mbelledonne@gmail.com>
2021-12-14 08:49:24 -0500

    added predicted location

--- src/utils/analysis_loading_utils.jl
+++ src/utils/analysis_loading_utils.jl
@@ -96,25 +96,24 @@ function chain_attention(chain, path;
 
     traces = chain.state.traces
     # TODO: generalize across kernel states
-    pf_st = Matrix{CausalGraph}(undef, np, k)
+    np = length(traces)
+    pf_st = Matrix{CausalGraph}(undef, np, steps)
     for i = 1:np
         # pf_st[i, :] = map(world, last(get_retval((traces[i]))))
         pf_st[i, :] = @>> get_retval(traces[i]) last map(world)
     end
-
     df = DataFrame(
                    frame = Int64[],
                    tracker = Int64[],
                    cycles = Int64[],
                    pred_x = Float64[],
                    pred_y = Float64[])
-    cgs = map(world, chain.
     for frame = 1:steps
         cycles_per_part = collect(values(aux_state[frame].allocated))
         # assuming all particles are aligned wrt tracker ids for now
         cpt = isempty(cycles_per_part) ? zeros(n_targets) : sum(cycles_per_part)
         np = size(pf_st, 1)
-        positions = Matrix{Float64}(undef, 3, n_targets, np)
+        positions = Array{Float64, 3}(undef, 3, n_targets, np)
         for p = 1:np
             trackers = get_objects(pf_st[p, frame], Dot)
             for i = 1:n_targets 
@@ -122,7 +121,7 @@ function chain_attention(chain, path;
             end
         end
         for i = 1:n_targets
-            px, py, _ = mean(positions[:, i, :], dims = 1)
+            px, py, _ = mean(positions[:, i, :], dims = 2)
             push!(df, (frame, i, cpt[i], px, py))
         end
     end
