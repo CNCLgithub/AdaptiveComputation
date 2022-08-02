90023f6e6a9b10badc2b5c6f60723b7beedf047a
belledon <mbelledonne@gmail.com>
2022-08-02 09:06:07 -0400

    staging for milgram

--- scripts/inference/exp2_probes/exp2_probes.jl
+++ scripts/inference/exp2_probes/exp2_probes.jl
@@ -5,9 +5,9 @@ using Gen_Compose
 using ArgParse
 using Accessors
 
-using Random
-Random.seed!(1234);
-@warn "Seed is set, remove before experiments!"
+# using Random
+# Random.seed!(1234);
+# @warn "Seed is set, remove before experiments!"
 
 # using Profile
 # using StatProfilerHTML
@@ -195,8 +195,8 @@ function main()
     c = args["chain"]
     # scene, chain, time
 
-    # cmd = ["$(i)", "$c", "T"]
-    cmd = ["$(i)", "$c", "-v", "-r", "--time=30", "T"]
+    cmd = ["$(i)", "$c", "T"]
+    # cmd = ["$(i)", "$c", "-v", "-r", "--time=30", "T"]
     run(cmd);
 end
 
