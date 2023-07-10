fbc75c671923821ae994b1b1298745ffd6b797ce
belledon <mbelledonne@gmail.com>
2023-07-10 16:12:08 -0400

    using unweighted samples for position digest

--- src/inference/procedures/latent_maps.jl
+++ src/inference/procedures/latent_maps.jl
@@ -5,6 +5,7 @@ function digest_auxillary(c::PFChain)
 end
 
 function digest_tracker_positions(c::PFChain)
+    # number of particles and targets
     np = length(c.state.traces)
     nt = @> (c.state.traces) begin
         first
@@ -13,9 +14,10 @@ function digest_tracker_positions(c::PFChain)
         get_objects
         length # num targets
     end
+    traces = sample_unweighted_traces(c.state, np)
     pos = Array{Float64, 3}(undef, np, nt, 2)
     for i = 1:np
-        (_, states) = Gen.get_retval(c.state.traces[i])
+        (_, states) = Gen.get_retval(traces[i])
         trackers = @> states last get_objects
         for j = 1:nt
             pos[i, j, :] = get_pos(trackers[j])
