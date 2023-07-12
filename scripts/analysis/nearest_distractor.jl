f49b0675d5653a29457499f8419fa2870e90376d
belledon <mbelledonne@gmail.com>
2023-07-12 10:24:26 -0400

    updated nearest distractor script

--- scripts/analysis/nearest_distractor.jl
+++ scripts/analysis/nearest_distractor.jl
@@ -24,22 +24,29 @@ function load_gt_positions(path::String)
         steps = scenes[s]["positions"]
         nt = length(steps)
         ni = length(scenes[s]["aux_data"]["targets"])
-        for t = 1:nt, i = 5:ni
+        for t = 1:nt, i = 1:ni
             x,y,_ = steps[t][i]
             push!(df, (s, t, i, x, y))
         end
     end
-    gdf = groupby(df, Cols(:scene, :frame))
-    return gdf
+    return df
 end
 
 function nearest_distractor_distance(scene, frame, tx, ty, gt_gd)
-    println("scene $scene, frame $frame")
     g = gt_gd[(scene = scene, frame = frame)]
     l2d = (x,y) -> l2_distance(tx,ty,x,y)
     transform!(g,
                [:x, :y] => l2d => :d)
     minimum(g.d)
+end
+
+
+function localization_error(scene, frame, tracker, tx, ty, gt_gd)
+    g = gt_gd[(scene = scene, frame = frame, object = tracker)]
+    l2d = (x,y) -> l2_distance(tx,ty,x,y)
+    transform!(g,
+               [:x, :y] => l2d => :d)
+    first(g.d)
 end
 
 function weighted_centroid(tracker, weight, pred_x, pred_y)
@@ -62,25 +69,53 @@ function l2_distance(cx, cy, x, y)
     @. sqrt((cx - x)^2 + (cy - y)^2)
 end
 
-# see `scripts/aggregate_chains.jl`
-model = "td"
-model_inferences = "/spaths/experiments/exp2_probes_adaptive_computation_$(model)_att.csv"
-df = DataFrame(CSV.File(model_inferences))
-df = select(df, Cols(:chain, :scene, :frame, :tracker, :pred_x, :pred_y))
-# filter!(row -> row.scene == 1, df) # TODO: remove after debugging
 
 # probe timing for experiment
 probe_timings = "/spaths/datasets/exp2_probe_map_random.csv"
 pdf = DataFrame(CSV.File(probe_timings))
+filter!(row -> row.scene <= 40, pdf)
+# probe frames
+probed_frames = select(pdf, Cols(:scene, :frame))
+
+# see `scripts/aggregate_chains.jl`
+model = "td"
+# chain performance
+model_perf_csv = "/spaths/experiments/exp2_probes_adaptive_computation_$(model)_perf.csv"
+# model_perf_csv = "/spaths/experiments/exp2_probes_adaptive_computation_perf.csv"
+model_perf = DataFrame(CSV.File(model_perf_csv))
+model_perf = groupby(model_perf, Cols(:chain, :scene))
+model_perf = combine(model_perf, Cols(:td_acc) => mean => :avg_acc)
+transform!(model_perf, [:avg_acc] => ByRow(acc -> acc > 0.75) => :passed)
+filter!(row -> row.passed, model_perf)
+passed_chains = select(model_perf, Cols(:chain, :scene))
+
+
+model_inferences = "/spaths/experiments/exp2_probes_adaptive_computation_$(model)_att.csv"
+# model_inferences = "/spaths/experiments/exp2_probes_adaptive_computation_att.csv"
+df = DataFrame(CSV.File(model_inferences))
+df = leftjoin(passed_chains, df, on = [:chain, :scene])
+df = select(df, Cols(:chain, :scene, :frame, :tracker, :pred_x, :pred_y))
+df = leftjoin(probed_frames, df, on = [:scene, :frame])
+# filter!(row -> row.scene == 1, df) # TODO: remove after debugging
+
 
 # distance to the nearest distractor for each frame x tracker
 gt_positions = load_gt_positions("/spaths/datasets/exp2_probes.json")
-nd_dist_f = ByRow((s, f, x, y) -> nearest_distractor_distance(s,f,x,y,gt_positions))
+
+grouped_gt_positions = groupby(gt_positions, Cols(:scene, :frame, :object))
+loc_error_f = ByRow((s, f, t, x, y) -> localization_error(s,f,t,x,y,grouped_gt_positions))
+transform!(df,
+           [:scene, :frame, :tracker, :pred_x, :pred_y] => loc_error_f => :loc_error)
+
+distractor_positions = filter(row -> row.object > 4, gt_positions)
+grouped_dis_positions = groupby(distractor_positions, Cols(:scene, :frame))
+nd_dist_f = ByRow((s, f, x, y) -> nearest_distractor_distance(s,f,x,y,grouped_dis_positions))
 transform!(df,
            [:scene, :frame, :pred_x, :pred_y] => nd_dist_f => :nn_dist)
 
+
 gdf = groupby(df, Cols(:scene, :frame, :tracker))
-gdf = combine(gdf, Cols(:pred_x, :pred_y, :nn_dist) .=> [mean std])
+gdf = combine(gdf, Cols(:pred_x, :pred_y, :loc_error, :nn_dist) .=> [mean std])
 
 CSV.write("/spaths/experiments/exp2_probes_adaptive_computation_$(model)_dnd.csv",
           gdf)
@@ -133,11 +168,5 @@ transform!(distances,
 # scene   chain   frame   ic_x  ic_y   d_ic
 select!(distances, Not(Cols(:pred_x, :pred_y)))
 display(distances)
-
-# a sanity check, looking at all chains on one scene
-s1 = filter(x -> x.scene == 1, distances)
-plt = scatterplot(s1.frame, s1.d_ic,
-                  height = 50, width = 50)
-display(plt)
 
 CSV.write("/spaths/experiments/exp2_probes_adaptive_computation_$(model)_dnd_centroid.csv", distances)
