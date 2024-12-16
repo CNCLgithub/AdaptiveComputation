7eba2b5bdf2decb12757c4b088acae572fbae81f
belledon <mbelledonne@gmail.com>
2024-12-16 11:36:52 -0500

    renamed grid search scripts

---
+++ scripts/experiments/probes/grid_search_tr.jl
@@ -0,0 +1,127 @@
+using CSV
+using MOT
+using ArgParse
+using Accessors
+using Gen_Compose
+using Base.Iterators: product
+
+experiment_name = "exp_probes"
+plan = :td
+
+t_pi = range(0.5, 4.0, 4)
+t_im = range(5.0, 20.0, 4)
+grid_search_steps = collect(product(t_pi, t_im));
+
+exp_params = (;experiment_name = experiment_name,
+              gm = "$(@__DIR__)/gm.json",
+              proc = "$(@__DIR__)/proc.json",
+              att = "$(@__DIR__)/$(plan).json",
+              dataset = "/spaths/datasets/$(experiment_name).json",
+              dur = 480, # number of frames to run; full = 480
+              model = "ac",
+              # SET FALSE for full experiment
+              restart = false,
+              viz = false,
+              # restart = true,
+              # viz = true,
+              )
+
+function run_model(scene::Int, chain::Int, gs_step::Int,
+                   tau_pi::Float64, tau_im::Float64)
+    gm = dgp_gm = MOT.load(InertiaGM, exp_params.gm)
+    # loading scene data
+    scene_data = MOT.load_scene(dgp_gm,
+                                exp_params.dataset,
+                                scene)
+    init_gt_state = scene_data[:gt_states][1]
+    gt_states = scene_data[:gt_states][2:(exp_params.dur + 1)]
+    aux_data = scene_data[:aux_data]
+
+    gm = setproperties(gm,
+                       (n_dots = gm.n_targets + aux_data["n_distractors"],
+                        vel = aux_data["vel"] * 0.55))
+
+    query = query_from_params(gm, init_gt_state, gt_states)
+
+    att = MOT.load(PopSensitivity,
+                   exp_params.att,
+                   plan = td_flat,
+                   plan_args = (tau_pi,),
+                   percept_update = tracker_kernel,
+                   percept_args = (3,), # look back steps
+                   importance_tau = tau_im,
+                   )
+    proc = MOT.load(PopParticleFilter,
+                    exp_params.proc;
+                    attention = att)
+
+    base_path = "/spaths/experiments/$(experiment_name)-gridsearch-tr"
+    path = "$(base_path)/$(gs_step)/$(scene)"
+    try
+        isdir(path) || mkpath(path)
+    catch e
+        println("could not make dir $(path)")
+    end
+
+    nsteps = length(gt_states) + 1
+    logger = MemLogger(nsteps)
+    chain_perf_path = joinpath(path, "$(chain)_perf.csv")
+    chain_att_path = joinpath(path, "$(chain)_att.csv")
+
+    println("running chain $(chain)")
+    if isfile(chain_perf_path) && exp_params.restart
+        rm(chain_perf_path)
+        rm(chain_att_path)
+    end
+    smc_chain = run_chain(proc, query, nsteps, logger)
+
+    dg = extract_digest(logger)
+    perf_df = MOT.chain_performance(dg)
+    perf_df[!, :scene] .= scene
+    perf_df[!, :chain] .= chain
+    CSV.write(chain_perf_path, perf_df)
+    att_df = MOT.chain_attention(dg, gm.n_targets)
+    att_df[!, :scene] .= scene
+    att_df[!, :chain] .= chain
+    CSV.write(chain_att_path, att_df)
+
+    exp_params.viz && visualize_inference(smc_chain, dg, gt_states, gm,
+                                          joinpath(path, "$(chain)_scene"))
+    return nothing
+end
+
+function pargs()
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+
+        "gs_step"
+        help = "Grid-search step"
+        arg_type = Int64
+        default = 16
+
+        "scene"
+        help = "Which scene to run"
+        arg_type = Int64
+        default = 32
+
+        "chains"
+        help = "Number of chains"
+        arg_type = Int64
+        default = 10
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    args = pargs()
+    t_pi, t_im = grid_search_steps[args["gs_step"]]
+    for ci = 1:args["chains"]
+        run_model(args["scene"], ci, args["gs_step"], x0, m);
+        GC.gc() # memory leak?
+    end
+end
+
+
+main();
